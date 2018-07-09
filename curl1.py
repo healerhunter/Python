9e05ea90b592bb87fee1188f424cb24f610ed864
healerhunter <40900477+healerhunter@users.noreply.github.com>
2018-07-09 13:16:29 +0530

    Update curl1.py

--- curl1.py
+++ curl1.py
@@ -1,6 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 
-img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg').read()
+img = urllib.request.urlopen('image add').read()
 fhand = open('cover3.jpg', 'wb')
 fhand.write(img)
 fhand.close()
